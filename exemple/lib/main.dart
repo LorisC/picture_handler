74d7345edd1d9a3fa061198e4f24d8d78b342504
Loris Ciprin <loris@ciprin.fr>
2020-05-04 17:09:17 +0200

    add path provider

--- exemple/lib/main.dart
+++ exemple/lib/main.dart
@@ -4,19 +4,21 @@ import 'package:picture_handler/picture_handler.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: SafeArea(child: AdvancedCameraPreview()),
+        body: SafeArea(
+            child: AdvancedCameraPreview(
+          pictureCallback: (String path) {
+            //Todo do something with the path
+          },
+        )),
       ),
     );
   }
 }
-
--- lib/picture_handler.dart
+++ lib/picture_handler.dart
@@ -1,9 +1,13 @@
 library picture_handler;
 
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
+import 'package:path_provider/path_provider.dart';
+typedef TackPictureCallback(String picturePath);
 class AdvancedCameraPreview extends StatefulWidget {
+  final TackPictureCallback pictureCallback;
+  const AdvancedCameraPreview({this.pictureCallback}) : assert(pictureCallback != null);
   @override
   _AdvancedCameraPreviewState createState() => _AdvancedCameraPreviewState();
 }
@@ -51,17 +55,20 @@ class _AdvancedCameraPreviewState extends State<AdvancedCameraPreview> {
             Positioned(
               bottom: 16,
               left: screenCenter - shooterRadius / 2,
-              child: Material(
-                color: Colors.white,
-                shape: CircleBorder(
-                  side: BorderSide(
-                      color: Colors.white, width: 2, style: BorderStyle.solid),
+              child: GestureDetector(
+                onTap: _takePicture,
+                child: Material(
+                  color: Colors.white,
+                  shape: CircleBorder(
+                    side: BorderSide(
+                        color: Colors.white, width: 2, style: BorderStyle.solid),
+                  ),
+                  child: Container(
+                    height: shooterRadius,
+                    width: shooterRadius,
+                  ),
+                  elevation: 16,
                 ),
-                child: Container(
-                  height: shooterRadius,
-                  width: shooterRadius,
-                ),
-                elevation: 16,
               ),
             )
           ],
@@ -70,9 +77,12 @@ class _AdvancedCameraPreviewState extends State<AdvancedCameraPreview> {
     );
   }
 
-  Future<void> _take_picture() async {
-
+  Future<void> _takePicture() async {
+    String path = (await getApplicationDocumentsDirectory()).path + '${DateTime.now().toIso8601String()}';
+    await _camera.takePicture(path);
+    widget.pictureCallback(path);
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
